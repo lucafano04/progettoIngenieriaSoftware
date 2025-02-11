c6d828f03275d48982858ab787c008f2b69430ef
Luca Facchini <45771235+lucafano04@users.noreply.github.com>
2025-02-11 11:33:26 +0100

    fix(bug): :bug: Corrected some routes to match API doc

--- app/routes/session.ts
+++ app/routes/session.ts
@@ -75,13 +75,15 @@ router.post('/', async (req, res) => {
 });
 router.get('/', token.checker, async (req, res) => {
     const { user } = req.body;
-    res.status(200).json({
+    const response: Utenti.User = {
+        self: user.self,
         email: user.email,
         nome: user.nome,
         cognome: user.cognome,
         ruolo: user.ruolo,
         imageUrl: user.imageUrl
-    });
+    };
+    res.status(200).json(response);
 });
 
 router.delete('/', token.checker, async (req, res) => {
